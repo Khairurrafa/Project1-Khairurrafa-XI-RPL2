ec9d44377494b6958231d524e82985fdf67a44dd
Khairurrafa <erlangga.rafa022@gmail.com>
2021-03-07 17:21:38 +0700

    Modul D: Pembuatan Pesanan

--- AplikasiRestaurant/src/Classes/DaftarMenu.java
+++ AplikasiRestaurant/src/Classes/DaftarMenu.java
@@ -1,6 +1,8 @@
 package Classes;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class DaftarMenu {
     private ArrayList<Menu> daftarMenu;
@@ -30,5 +32,71 @@ public class DaftarMenu {
         getMenuByKategori("Kuah");
         getMenuByKategori("Toping");
         getMenuByKategori("Minuman");
+    }
+
+    //tambah method
+    public Menu pilihMenu() {
+        try{
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Nomor Menu yang dipesan : ");
+            int no_menu = input.nextInt();
+
+            //get menu berdasarkan no_menu, di -1 karena arrayList mulai dari 0
+            Menu m = daftarMenu.get(no_menu-1);
+
+            //cek apakah menu kuah?
+            if (!m.getKategori().equalsIgnoreCase("Kuah")) {
+                return m;
+            }else {
+                //jika yang dipilih adalah menu kuah, maka tidak bisa, user harus memilih lagi
+                System.out.println("[Err] Pesan dulu Menu Ramen");
+                return pilihMenu();
+            }
+        } catch (IndexOutOfBoundsException err) {
+            //jika no_menu tidak ada, makan akan masuk ke sini
+            //mo_menu dianggap tidak ada ketika no_menu diluar dari index arrayList
+
+            System.out.println("[Err] Pesanan Tidak Tersedia");
+            //jika tidak da, maka user akan diminta untuk mengulang memasukan nomor menu
+            //teknik ini disebut dengan rekursif
+            return pilihMenu();
+        } catch (InputMismatchException err) {
+            //jika input bukan berupa angka maka akan masuk ke sini
+            System.out.println("[Err] Mohon masukan nomor menu");
+            return pilihMenu();
+        }
+    }
+
+    public Menu pilihKuah () {
+        try {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Kuah [sesuai nomor menu] : ");
+            int no_menu == input.nextInt();
+
+            //get menu berdasarkan no_menu, di -1 karena arrayList mulai dari 0
+            Menu m = daftarMenu.get(no_menu-1);
+
+            //cek apakah menu kuah?
+            if (!m.getKategori().equalsIgnoreCase("Kuah")) {
+                return m;
+            }else {
+                System.out.println("[Err] Bukan Menu Kuah");
+                return pilihKuah();
+            }
+        } catch (IndexOutOfBoundsException err) {
+            //jika no_menu tidak ada, makan akan masuk ke sini
+            //mo_menu dianggap tidak ada ketika no_menu diluar dari index arrayList
+
+            System.out.println("[Err] Pesanan Tidak Tersedia");
+            //jika tidak da, maka user akan diminta untuk mengulang memasukan nomor menu
+            //teknik ini disebut dengan rekursif
+            return pilihKuah();
+        } catch (InputMismatchException err) {
+            //jika input bukan berupa angka maka akan masuk ke sini
+            System.out.println("[Err] Mohon masukan nomor kuah");
+            return pilihKuah();
+        }
     }
 }
--- AplikasiRestaurant/src/Classes/Transaksi.java
+++ AplikasiRestaurant/src/Classes/Transaksi.java
@@ -11,14 +11,28 @@ public class Transaksi {
     private double uangBayar;
     private double pajak;
     private double totalBayar;
+    //tambah
+    private double biayaService=0;
 
-    public Transaksi(String no_transaksi, String nm_pemesan, String tanggal, String no_meja) { }
+    public Transaksi(String no_transaksi, String nm_pemesan, String tanggal, String no_meja) {
+        this.noTransaksi = no_transaksi;
+        this.namaPemesan = nm_pemesan;
+        this.tanggal = tanggal;
+        this.noMeja = no_meja;
 
-    public void tambahPesanan(Pesanan pesanan) { }
+        pesanan = new ArrayList<>();
+    }
 
-    public Pesanan getPesanan() { return null; }
+    public void tambahPesanan(Pesanan pesanan) {
+        this.pesanan.add(pesanan);
+    }
 
-    public ArrayList<Pesanan> getSemuaPesanan() { return null; }
+    //hilangkan dari struktur kelas
+    // public Pesanan getPesanan() {
+    // return pesanan;
+    // }
+
+    public ArrayList<Pesanan> getSemuaPesanan() { return pesanan; }
 
     public double hitungTotalBayar() { return 0; }
 
